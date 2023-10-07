8fc57d2619c6f0916111f127e3b77c68be99800d
Niket Yende <niket6@gmail.com>
2023-10-07 10:35:01 +0530

    Update hardhat config

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.10;
 
 import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
